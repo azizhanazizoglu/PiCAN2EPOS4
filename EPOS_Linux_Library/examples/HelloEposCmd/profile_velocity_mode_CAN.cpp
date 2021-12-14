e1407f8155e68c95170506314800016df1f519d9
ga86yel <azizhan.azizoglu@tum.de>
2021-12-14 16:11:07 +0100

    std::int8_t OpMode = 0;

--- EPOS_Linux_Library/examples/HelloEposCmd/profile_velocity_mode_CAN.cpp
+++ EPOS_Linux_Library/examples/HelloEposCmd/profile_velocity_mode_CAN.cpp
@@ -1023,14 +1023,13 @@ int RunProfileVelocityMode(unsigned int* p_pErrorCode)
 	lResult = ProfileVelocityMode(g_pKeyHandle, g_usNodeId_1,g_usNodeId_2, lErrorCode);
 	//std::cout<<"ProfileVelocityMode Node Check Node1 "<<g_usNodeId_1<<"Node2 "<<g_usNodeId_2<< "tv1 "<<targetvelocity_1<<"tv2 "<<targetvelocity_2<<endl;
 
-	char OpMode = 0;	
+	std::int8_t OpMode = 0;	
 	if(VCS_GetOperationMode(g_pKeyHandle, g_usNodeId_1, &OpMode,&lErrorCode) == 0)
 	{
 		LogError("VCS_GetOperationMode Fault", lResult, lErrorCode);
 		lResult = MMC_FAILED;
 	}
 	std::cout<<"VCS_GetOperationMode : "<<OpMode<<endl;	
-	printf(*OpMode);
 	if(lResult != MMC_SUCCESS)
 	{
 		LogError("ProfileVelocityMode_Node1&Node2", lResult, lErrorCode);
