2352cdae1c98e4d2b6e5b1234551cb4bfbc96c91
ga86yel <azizhan.azizoglu@tum.de>
2021-12-15 13:32:11 +0100

    p_rlErrorCode

--- EPOS_Linux_Library/examples/HelloEposCmd/profile_velocity_mode_CAN.cpp
+++ EPOS_Linux_Library/examples/HelloEposCmd/profile_velocity_mode_CAN.cpp
@@ -133,7 +133,7 @@ int ProfileVelocityModeSettings(HANDLE p_DeviceHandle, unsigned short p_usNodeId
 	
 	//Get Parameter (B)
 	int MaxProfileVelocity;
-	if(VCS_GetObject(p_DeviceHandle, p_usNodeId, 0x607F,0x00, &MaxProfileVelocity, 4,&pNbOfBytesWritten, &p_rlErrorCode) == 0)
+	if(VCS_GetObject(p_DeviceHandle, p_usNodeId, 0x607F,0x00, &MaxProfileVelocity, 4,&pNbOfBytesWritten, p_rlErrorCode) == 0)
 	{
 		lResult = MMC_FAILED;
 		LogError("VCS_GetObject 0x6061", lResult, p_rlErrorCode);
