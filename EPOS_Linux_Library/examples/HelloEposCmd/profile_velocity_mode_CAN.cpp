f133ed096c99fe7efc1c372066b7e623b089373e
ga86yel <azizhan.azizoglu@tum.de>
2021-12-16 01:53:16 +0100

    targetvelocity_2

--- EPOS_Linux_Library/examples/HelloEposCmd/profile_velocity_mode_CAN.cpp
+++ EPOS_Linux_Library/examples/HelloEposCmd/profile_velocity_mode_CAN.cpp
@@ -53,7 +53,8 @@ const string g_programName = "HelloEposCmd";
 
 //Profile Velocity Default Inputs
 long targetvelocity_1 = 0; //rpm
-int TargetTorqueNode2 = 0; //rpm
+long targetvelocity_2 = 0; //rpm
+int TargetTorqueNode2 = 0; //1000x * Motor Rated Torque
 long double simtime = 0;
 vector<double> p_CurrentIs_saved;
 vector<double> p_Time_saved;
