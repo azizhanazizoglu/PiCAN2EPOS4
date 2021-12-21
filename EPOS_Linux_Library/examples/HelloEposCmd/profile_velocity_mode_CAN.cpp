6e31229ae54ead9bddc7723b56a4eb9b842679a9
ga86yel <azizhan.azizoglu@tum.de>
2021-12-21 20:04:04 +0100

    std::cout <<TesProfileDatas.at(3).first<<endl; ---previous works----

--- EPOS_Linux_Library/examples/HelloEposCmd/profile_velocity_mode_CAN.cpp
+++ EPOS_Linux_Library/examples/HelloEposCmd/profile_velocity_mode_CAN.cpp
@@ -1621,7 +1621,7 @@ int main(int argc, char** argv)
 	std::cout <<TesProfileDatas.size()<<endl;
 	for ( auto i = 0 ; i < 1 ; i ++)
 	{
-		std::cout <<TesProfileDatas.at(1).first<<endl;
+		std::cout <<TesProfileDatas.at(3).first<<endl;
 	}
 	
 
